265162b3219c25a02709f40af33ae7b99fa9c1b3
David Trompa <dacardo4@gmail.com>
2023-09-10 17:48:54 -0500

    http get

--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
 
-  constructor() { }
+  private urlApi = 'https://rickandmortyapi.com/api/';
+
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  httpGet(url: string): any {
+    return this._http.get(`${this.urlApi}/${url}`);
+  }
 }
